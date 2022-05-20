81b4a9e5b3fb4729aa1ec50a7ccdab9e8a12fae3
Wojciech Glegola <wglegola@gmail.com>
2022-05-20 18:04:59 +0200

    added list component funcionality

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipeList: Recipe[] = [new Recipe("Test","A TestXD", "https://imgs.search.brave.com/kFuOJitvW3VfteX4w3DFI9-Ck2IW2MRhil6lrK3B2Lg/rs:fit:905:225:1/g:ce/aHR0cHM6Ly90c2Uy/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5k/NGlwa2wtUEtHcGVU/ZDhkQ2xuRGxnSGFE/NCZwaWQ9QXBp")];
   constructor() { }
 
   ngOnInit(): void {

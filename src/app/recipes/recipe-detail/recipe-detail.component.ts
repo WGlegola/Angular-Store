5cccba9722d64cde149302bb89a900017d4b38e4
Wojciech Glegola <wglegola@gmail.com>
2022-06-02 20:50:26 +0200

    added routiong

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 import { Ingredient } from 'src/app/shared/ingredient.model';
 import { ShoppingListService } from 'src/app/shopping-list/shopping-list.service';
 
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -10,11 +12,20 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-detail.component.css']
 })
 export class RecipeDetailComponent implements OnInit {
-  @Input() recipe: Recipe;
-
-  constructor(private shoppingListService:ShoppingListService) { }
+  recipe: Recipe;
+  id:number;
+  constructor(
+    private shoppingListService:ShoppingListService,
+    private recipeService: RecipeService,
+    private route:ActivatedRoute) { }
 
   ngOnInit() {
+    this.route.params.subscribe(
+      (params:Params)=> {
+        this.id = +params['id'];
+        this.recipe = this.recipeService.getRecipeById(this.id);
+      }
+    );
   }
 
   onAddToShoppingList(){
--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -11,14 +11,10 @@ import { RecipeService } from '../../recipe.service';
 export class RecipeItemComponent implements OnInit {
   @Input() recipe: Recipe;
   @Output() recipeSelected = new EventEmitter<void>();
-
+  @Input() index: number;
   constructor(private recipeService: RecipeService) { }
 
   ngOnInit() {
-  }
-
-  onSelected() {
-    this.recipeService.recipeSelected.emit(this.recipe);
   }
 
 }
